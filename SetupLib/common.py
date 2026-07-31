d7cc3a5ab834b82704729fe8ca1be541dfcaf36d
CraftierBark8 <h3xl5hs1@duck.com>
2026-07-31 01:49:04 -0400

    Added functions to common

---
+++ SetupLib/common.py
@@ -0,0 +1,62 @@
+# A list of common functions for setup
+
+def selector(options):
+# Select from options
+    # Print options
+    print()
+    for i in range(len(options)):
+        print("(" + str(i + 1) + ") - " + options[i])
+
+    # Ask for input and validate
+    while True:
+        selection = input("Please Select an option (1 to " + str(len(options)) + "): ")
+        # Validate
+        try:
+            selection = int(selection)
+            selection = selection - 1
+            if selection > -1 and selection < (len(options) + 1):
+                return selection
+            else:
+                print("Error - Please select an option between 1 and " + str(len(options)))
+        except "InvalidOption":
+            print("Error - Please select an option between 1 and " + str(len(options)))
+
+def YN(message, default = ""):
+# Do a y/n message
+    # Select default
+    # Default Y
+    if default == "Y" or default == "y":
+        # Input and validate
+        while True:
+            print()
+            selection = input(message + " [Y/n]: ")
+            if selection == "" or selection == "Y" or selection == "y":
+                return True
+            elif selection == "N" or selection == "n":
+                return False
+            else:
+                print("Error - Invalid selection. Please enter a Y or N")
+    # Default N
+    elif default == "N" or default == "n":
+        # Input and validate
+        while True:
+            print()
+            selection = input(message + " [y/N]: ")
+            if selection == "Y" or selection == "y":
+                return True
+            elif selection == "" or selection == "N" or selection == "n":
+                return False
+            else:
+                print("Error - Invalid selection. Please enter a Y or N")
+    # Default none
+    else:
+        # Input and validate
+        while True:
+            print()
+            selection = input(message + " [Y/n]: ")
+            if selection == "Y" or selection == "y":
+                return True
+            elif selection == "N" or selection == "n":
+                return False
+            else:
+                print("Error - Invalid selection. Please enter a Y or N")
