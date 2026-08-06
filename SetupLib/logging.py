96dc7035a3a2e11e56a2270b23dbe8969495088a
CraftierBark8 <h3xl5hs1@duck.com>
2026-08-05 20:22:32 -0400

    Removed 'log_name' parameter

--- SetupLib/logging.py
+++ SetupLib/logging.py
@@ -1,13 +1,35 @@
-# Logging Functions for OS-Setup
+'''
+OS-Setup - logging.py
+This program's purpose is to dynamically create a an executable bash script that installs 
+    and configures the programs selected. This module provides logging functions for main
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(c) 2026 CraftierBark8 on GitHub
+Contact me via GitHub or email at h3xl5hs1@duck.com
+'''
 
 # import
 import datetime
+from pathlib import Path
 
-# Start new log
+# Start new log and log folder
 def log_start():
-    # Create log
+    # Try to create log
     try:
         current_time = str(datetime.datetime.now())
+        global log_name
         log_name = str(current_time + " - OS-Setup.log")
         log_name = log_name.replace(":", ".")
         active_log = open(log_name, "a+")
@@ -21,7 +43,7 @@ def log_start():
 
 # Add warning to log
 # Desc: Adds a warning to the log, optionally writes to terminal (default True)
-def log_warn(log_name, message, write_term = True):
+def log_warn(message, write_term = True):
     current_time = str(datetime.datetime.now())
     try:
         active_log = open(log_name, "a+")
@@ -35,20 +57,19 @@ def log_warn(log_name, message, write_term = True):
 
 # Exit with error and log
 # Desc: Exit with a fatal error, incl. log_name and message, always writes to terminal
-def log_fatal(log_name, message):
+def log_exception(exception, message):
     current_time = str(datetime.datetime.now())
     try:
         active_log = open(log_name, "a+")
-        active_log.write(current_time + " - Fatal Error: " + message + "\n")
+        active_log.write(current_time + " - " + exception + ": " + message + "\n")
     except:
         1 + 1
     finally:
         active_log.close()
-        exit("Fatal Error: " + message)
 
 # Log event
 # Desc: Adds an event to the log, optionally writes event to terminal (default false)
-def log_event(log_name, message, write_term = False):
+def log_event(message, write_term = False):
     current_time = str(datetime.datetime.now())
     try:
         active_log = open(log_name, "a+")
@@ -63,7 +84,7 @@ def log_event(log_name, message, write_term = False):
 
 # Log exit
 # Desc: Logs an intended exit of the program, always writes to terminal
-def log_exit(log_name, message):
+def log_exit(message):
     current_time = str(datetime.datetime.now())
     try:
         active_log = open(log_name, "a+")
